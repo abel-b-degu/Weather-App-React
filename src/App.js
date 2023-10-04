46c306cb0a6bf462b2b6940c0a3b767802dd3dfb
abelbirhanu1057@gmail.com <113086309+abel-b-degu@users.noreply.github.com>
2023-10-03 21:07:35 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React , {useState} from 'react';
+import axios from axios;
+
+function App() {
+
+// const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=4d6b9a5f50de2001a5889910ea90be7d`
+ 
+return (
+    <div className="App">
+   
+    </div>
+  );
+}
+
+export default App;
