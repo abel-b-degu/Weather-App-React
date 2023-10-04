052834a267f26c194be268dac04d4e048b1ffa8b
abelbirhanu1057@gmail.com <113086309+abel-b-degu@users.noreply.github.com>
2023-10-03 23:53:29 -0500

    Configured the main page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React , {useState} from 'react';
-import axios from axios;
+import axios from 'axios';
 
 function App() {
 
 // const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=4d6b9a5f50de2001a5889910ea90be7d`
  
 return (
-    <div className="App">
-   
+    <div className="app">
+      <div className='container'>
+        <div className='top'>
+          <div className='location'>
+            <p>Dallas</p>
+          </div>
+          <div className='temp'>
+            <h1>60°F</h1>   
+          </div>
+          <div className='description'>
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className='bottom'>
+          <div className='feels'>
+            <p>65°F</p>
+          </div>
+          <div className='humidity'>
+            <p>20%</p>
+          </div>
+          <div className='wind'>
+            12 MPH
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
